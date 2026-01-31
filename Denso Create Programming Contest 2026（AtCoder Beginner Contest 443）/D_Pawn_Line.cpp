1f37d25e4a23833dbf54a1151e41d26a22f6c067
qingzhixing <qzsqqmail@qq.com>
2026-01-31 21:31:32 +0800

    Fuck

--- Denso Create Programming Contest 2026（AtCoder Beginner Contest 443）/D_Pawn_Line.cpp
+++ Denso Create Programming Contest 2026（AtCoder Beginner Contest 443）/D_Pawn_Line.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <deque>
+#include <queue>
+#include <set>
 using namespace std;
 
 void Solution()
@@ -10,73 +11,53 @@ void Solution()
 	int n;
 	cin >> n;
 	vector<int> r;
+	set<int> value_set;
 
-	int lowest = 4e5 + 114;
 	for (int i = 0; i < n; i++)
 	{
 		int row;
 		cin >> row;
-		lowest = min(lowest, row);
 		r.push_back(row);
+		value_set.insert(row);
 	}
 
-	// BFS
-	deque<int> changing_idxs;
-	vector<bool> changed_idxs(n + 10);
-
-	// 查找所有最小值为lowest的节点依次入队作为BFS起点
-	for (int i = 0; i < n; i++)
+	priority_queue<int, vector<int>, greater<int>> value_queue;
+	for (auto value : value_set)
 	{
-		if (r[i] == lowest)
-		{
-			changed_idxs[i] = true;
-			changing_idxs.push_back(i);
-		}
+		value_queue.push(value);
 	}
 
-	while (!changing_idxs.empty())
+	while (value_queue.size())
 	{
-		auto current_idx = changing_idxs.front();
-		changing_idxs.pop_front();
-		// 激活相邻index
-		int left_idx = current_idx - 1, right_idx = current_idx + 1;
-		if (left_idx >= 0 && !changed_idxs[left_idx])
-		{
-			changed_idxs[left_idx] = true;
-			changing_idxs.push_back(left_idx);
+		auto value = value_queue.top();
+		value_queue.pop();
 
-			// 移动节点
-			int delta = r[left_idx] - r[current_idx];
-			if (delta > 1)
+		for (int i = 0; i < n; i++)
+		{
+			if (r[i] != value)
+				continue;
+			if (i - 1 > 0 && r[i - 1] - r[i] > 1)
 			{
-				// 移动到差一格就好
-				r[left_idx] = r[current_idx] + 1;
-				result += delta - 1;
+				result += r[i - 1] - r[i] - 1;
+				r[i - 1] = r[i] + 1;
+				if (value_set.find(r[i] + 1) == value_set.end())
+				{
+					value_set.insert(r[i] + 1);
+					value_queue.push(r[i] + 1);
+				}
 			}
-		}
-		if (right_idx < n && !changed_idxs[right_idx])
-		{
-			changed_idxs[right_idx] = true;
-			changing_idxs.push_back(right_idx);
-
-			// 移动节点
-			int delta = r[right_idx] - r[current_idx];
-
-			if (delta > 1)
+			if (i + 1 < n && r[i + 1] - r[i] > 1)
 			{
-				// 移动到差一格就好
-				r[right_idx] = r[current_idx] + 1;
-				result += delta - 1;
+				result += r[i + 1] - r[i] - 1;
+				r[i + 1] = r[i] + 1;
+				if (value_set.find(r[i] + 1) == value_set.end())
+				{
+					value_set.insert(r[i] + 1);
+					value_queue.push(r[i] + 1);
+				}
 			}
 		}
 	}
-	
-	for (int i = 0; i < n; i++)
-	{
-		cout << r[i] << ' ';
-	}
-	cout << endl;
-
 	cout << result << endl;
 }
 
