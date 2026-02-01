b06bddeaae4a2df76315263f8a5a3b06ec3cea77
qingzhixing <qzsqqmail@qq.com>
2026-02-01 12:42:16 +0800

    😥 long long 我恨你

--- Denso Create Programming Contest 2026（AtCoder Beginner Contest 443）/D_Pawn_Line.cpp
+++ Denso Create Programming Contest 2026（AtCoder Beginner Contest 443）/D_Pawn_Line.cpp
@@ -1,62 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <set>
 using namespace std;
 
 void Solution()
 {
-	int result = 0;
+	long long result = 0;
 
 	int n;
 	cin >> n;
-	vector<int> r;
-	set<int> value_set;
-
-	for (int i = 0; i < n; i++)
+	vector<long long> origin_row(n + 10);
+	vector<long long> row(n + 10);
+	for (int i = 1; i <= n; i++)
 	{
-		int row;
-		cin >> row;
-		r.push_back(row);
-		value_set.insert(row);
+		cin >> origin_row[i];
+		row[i] = origin_row[i];
 	}
-
-	priority_queue<int, vector<int>, greater<int>> value_queue;
-	for (auto value : value_set)
+	for (int i = 2; i <= n; i++)
 	{
-		value_queue.push(value);
+		row[i] = min(row[i], row[i - 1] + 1);
 	}
-
-	while (value_queue.size())
+	for (int i = n - 1; i >= 1; i--)
 	{
-		auto value = value_queue.top();
-		value_queue.pop();
-
-		for (int i = 0; i < n; i++)
-		{
-			if (r[i] != value)
-				continue;
-			if (i - 1 > 0 && r[i - 1] - r[i] > 1)
-			{
-				result += r[i - 1] - r[i] - 1;
-				r[i - 1] = r[i] + 1;
-				if (value_set.find(r[i] + 1) == value_set.end())
-				{
-					value_set.insert(r[i] + 1);
-					value_queue.push(r[i] + 1);
-				}
-			}
-			if (i + 1 < n && r[i + 1] - r[i] > 1)
-			{
-				result += r[i + 1] - r[i] - 1;
-				r[i + 1] = r[i] + 1;
-				if (value_set.find(r[i] + 1) == value_set.end())
-				{
-					value_set.insert(r[i] + 1);
-					value_queue.push(r[i] + 1);
-				}
-			}
-		}
+		row[i] = min(row[i], row[i + 1] + 1);
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		result += origin_row[i] - row[i];
 	}
 	cout << result << endl;
 }
